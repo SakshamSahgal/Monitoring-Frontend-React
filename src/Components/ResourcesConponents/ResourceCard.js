65629cc0082824a83871c857c08de8fcf8e0fcac
Saksham Sahgal <73836766+SakshamSahgal@users.noreply.github.com>
2024-01-14 19:18:08 +0530

    working .vbs settings

--- src/Components/ResourcesConponents/ResourceCard.js
+++ src/Components/ResourcesConponents/ResourceCard.js
@@ -1,8 +1,24 @@
 import { Button } from "react-bootstrap"
-import { AxiosGET } from "../../Scripts/AxiosRequest";
+import { AxiosPUT } from "../../Scripts/AxiosRequest";
+import { useState } from "react";
+import Cookies from "js-cookie";
 
-function ResourceCard({ Heading, APIPath}) {
+function ResourceCard({ Heading, APIPath }) {
 
+    const [data, setData] = useState({});
+
+    const SendDataToServer = async () => {
+        console.log(data);
+        const response = await AxiosPUT(APIPath, { data: data }, Cookies.get('token'));
+        console.log(response);
+    }
+
+    console.log(data);
+
+    // update the state when the user types in the input
+    const handleInputChange = (e) => {
+        setData(e.target.value);
+    };
 
     return (
         <>
@@ -19,14 +35,21 @@ function ResourceCard({ Heading, APIPath}) {
                         </div>
                         <div className="row">
                             <div className="col-12">
-                                <textarea className="form-control" rows="3">
+                                <textarea className="form-control bg-dark text-white" rows="3" onChange={handleInputChange}>
                                 </textarea>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div className="card-footer text-center">
-                    <Button> Save ✏️</Button>
+                    <div className="container">
+
+                        <div className="row">
+                            <div className="col-12">
+                                <Button onClick={SendDataToServer}> Save ✏️</Button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
